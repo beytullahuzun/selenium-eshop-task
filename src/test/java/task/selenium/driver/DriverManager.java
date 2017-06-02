6215a0069c68a2c5937fff9223c58ccfeb95a4c6
Sergey Shakur <sergejs.sakurs@4finance.com>
2017-06-02 13:26:48 +0300

    some refactoring - moved DriverPath to a more appropriate place

--- src/test/java/task/selenium/driver/DriverManager.java
+++ src/test/java/task/selenium/driver/DriverManager.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import task.selenium.utils.DriverPath;
 
 import java.io.File;
 import java.net.URISyntaxException;
--- src/test/java/task/selenium/driver/DriverPath.java
+++ src/test/java/task/selenium/driver/DriverPath.java
@@ -1,4 +1,4 @@
-package task.selenium.utils;
+package task.selenium.driver;
 
 import java.util.stream.Stream;
 
